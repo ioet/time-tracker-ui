4ec762b2090fb6b278f8e5421f5ce7d0f18c8446
daros10 <daro.wla@hotmail.es>
2020-03-10 15:19:11 -0500

    remove sub-foders

--- time-tracker/src/app/app.module.ts
+++ time-tracker/src/app/app.module.ts
@@ -4,23 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { TimeclockComponent } from './components/shared/clock/timeclock/timeclock.component';
-import { UserOptionsComponent } from './components/shared/user/user-options/user-options.component';
-import { SidebarOptionsComponent } from './components/shared/sidebar/sidebar-options/sidebar-options.component';
-import { ClockComponent } from './components/shared/clock/clock.component';
-import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 import { UserComponent } from './components/shared/user/user.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { ClockComponent } from './components/shared/clock/clock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    TimeclockComponent,
-    UserOptionsComponent,
-    SidebarOptionsComponent,
-    ClockComponent,
-    SidebarComponent,
     UserComponent,
+    SidebarComponent,
+    ClockComponent,
   ],
   imports: [
     BrowserModule,
