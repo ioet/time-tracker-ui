f39eac15d44ed943926f118a99098b2d41e47fd6
daros10 <daro.wla@hotmail.es>
2020-03-10 23:26:59 -0500

    added animation spinner and routes sidebar

--- src/app/components/shared/clock/clock.component.ts
+++ src/app/components/shared/clock/clock.component.ts
@@ -11,13 +11,27 @@ export class ClockComponent implements OnInit {
   hour: number;
   minutes: number;
   seconds: number;
+  displayTime: boolean;
 
   constructor() {
     this.currentDate = new Date();
     this.hour = this.currentDate.getHours();
     this.minutes = this.currentDate.getMinutes();
     this.seconds = this.currentDate.getSeconds();
+    this.displayTime = false;
+    setTimeout(() => {
+      this.displayTime = true;
+    }, 3000);
    }
+
+    horatTest() {
+      this.currentDate = new Date();
+      this.hour = this.currentDate.getHours();
+      this.minutes = this.currentDate.getMinutes();
+      this.seconds = this.currentDate.getSeconds();
+      this.displayTime = false;
+      setTimeout( 'horatTest()' , 3000);
+    }
 
 
   ngOnInit(): void {
