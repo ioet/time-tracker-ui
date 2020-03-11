47ee4f375e1daa4a2457d75e6e6dfa3f30e82f6b
daros10 <daro.wla@hotmail.es>
2020-03-10 22:43:52 -0500

    routes working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GettingStartedComponent } from './components/optionsSidebar/getting-started/getting-started.component';
+import { ReportsComponent } from './components/optionsSidebar/reports/reports.component';
+import { TimeClockComponent } from './components/optionsSidebar/time-clock/time-clock.component';
+import { TimeEntriesComponent } from './components/optionsSidebar/time-entries/time-entries.component';
+import { TimeOffComponent } from './components/optionsSidebar/time-off/time-off.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'gettingStarted', component: GettingStartedComponent},
+  {path: 'reports', component: ReportsComponent},
+  {path: 'timeClock', component: TimeClockComponent},
+  {path: 'timeEntries', component: TimeEntriesComponent},
+  {path: 'timeOff', component: TimeOffComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'gettingStarted'},
+  {path: '**', pathMatch: 'full', redirectTo: 'gettingStarted'},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
