c656f2c7553585a5852ab86e01453fd3431dc48d
daros10 <daro.wla@hotmail.es>
2020-03-15 21:42:46 -0500

    added code coverage dependecies

--- src/app/components/optionsSidebar/time-clock/time-clock.component.ts
+++ src/app/components/optionsSidebar/time-clock/time-clock.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeClockComponent implements OnInit {
 
-  constructor() { }
+  isClockIn: boolean;
+
+  constructor() {
+    this.isClockIn = false;
+   }
+
+   employeClockIn(): boolean {
+     this.isClockIn = true;
+     console.log('valor es' + this.isClockIn);
+     return this.isClockIn;
+   }
 
   ngOnInit(): void {
   }
