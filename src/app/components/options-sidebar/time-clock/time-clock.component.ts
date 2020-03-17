65b5195447ebbc509497bfdc3ea8be9d250d46f9
daros10 <daro.wla@hotmail.es>
2020-03-16 22:34:54 -0500

    added timer

--- src/app/components/options-sidebar/time-clock/time-clock.component.ts
+++ src/app/components/options-sidebar/time-clock/time-clock.component.ts
@@ -1,27 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, timer } from 'rxjs';
+
 
 @Component({
   selector: 'app-time-clock',
   templateUrl: './time-clock.component.html',
   styleUrls: ['./time-clock.component.css']
 })
-export class TimeClockComponent {
+export class TimeClockComponent implements OnInit {
 
   username = 'Dario';
   clockInUsername = 'hh:mm:ss';
   clockOutUsername = 'hh:mm:ss';
-
   isClockIn: boolean;
   isEnterTechnology: boolean;
   showAlertEnterTecnology: boolean;
 
+  hour: number;
+  minute: number;
+  seconds: number;
+
+  public timer;
+
   constructor() {
     this.isClockIn = true;
     this.isEnterTechnology = false;
+    this.hour = 0;
+    this.minute = 0;
+    this.seconds = 0;
    }
 
    employeClockIn(): boolean {
      this.isClockIn = !this.isClockIn;
+     this.enableTimer();
      return this.isClockIn;
    }
 
@@ -33,6 +44,7 @@ export class TimeClockComponent {
        this.isClockIn = true;
        this.isEnterTechnology = false;
        this.showAlertEnterTecnology = false;
+       console.log('Disble Timer');
      }
    }
 
@@ -43,4 +55,27 @@ export class TimeClockComponent {
        this.isEnterTechnology = false;
      }
    }
+
+   enableTimer() {
+    this.timer = interval(1000);
+    this.timer.subscribe( (data) => {
+      this.seconds += 1;
+      if ( this.seconds === 59 ) {
+        this.minute += 1;
+        this.seconds = 0;
+        if ( this.minute === 59 ) {
+          this.hour += 1;
+          this.minute = 0;
+        }
+      }
+      // console.log(this.hour + ' : ' + this.minute + ' : ' + this.seconds);
+    });
+
+   }
+
+
+   ngOnInit(): void {
+  }
+
+
 }
--- src/app/components/shared/clock/clock.component.ts
+++ src/app/components/shared/clock/clock.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, timer } from 'rxjs';
 
 @Component({
   selector: 'app-clock',
@@ -14,14 +15,21 @@ export class ClockComponent implements OnInit {
   displayTime: boolean;
 
   constructor() {
-    this.currentDate = new Date();
-    this.hour = this.currentDate.getHours();
-    this.minutes = this.currentDate.getMinutes();
-    this.seconds = this.currentDate.getSeconds();
+    this.showClcok();
     this.displayTime = false;
     setTimeout(() => {
       this.displayTime = true;
     }, 3000);
+   }
+
+   showClcok() {
+    const timer = interval(1000);
+    timer.subscribe( (data) => {
+      this.currentDate = new Date();
+      this.hour = this.currentDate.getHours();
+      this.minutes = this.currentDate.getMinutes();
+      this.seconds = this.currentDate.getSeconds();
+    } );
    }
 
   ngOnInit(): void {
