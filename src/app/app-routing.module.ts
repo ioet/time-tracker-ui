060d2c896695498644e47ff707e771b6823e20ea
Cristina Guncay <macrisguncay@gmail.com>
2020-03-23 22:57:18 -0500

    remove Getting Started and Time Off options in sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { GettingStartedComponent } from './components/options-sidebar/getting-started/getting-started.component';
 import { ReportsComponent } from './components/options-sidebar/reports/reports.component';
 import { TimeClockComponent } from './components/options-sidebar/time-clock/time-clock.component';
 import { TimeEntriesComponent } from './components/options-sidebar/time-entries/time-entries.component';
-import { TimeOffComponent } from './components/options-sidebar/time-off/time-off.component';
 import { ProjectManagementComponent } from './components/options-sidebar/project-management/project-management.component';
 
 const routes: Routes = [
-  {path: 'getting-started', component: GettingStartedComponent},
   {path: 'reports', component: ReportsComponent},
   {path: 'time-clock', component: TimeClockComponent},
   {path: 'time-entries', component: TimeEntriesComponent},
-  {path: 'time-off', component: TimeOffComponent},
   {path: 'project-management', component: ProjectManagementComponent},
-  {path: '', pathMatch: 'full', redirectTo: 'getting-started'},
-  {path: '**', pathMatch: 'full', redirectTo: 'getting-started'},
+  {path: '', pathMatch: 'full', redirectTo: 'time-clock'},
+  {path: '**', pathMatch: 'full', redirectTo: 'time-clock'},
 ];
 
 @NgModule({
