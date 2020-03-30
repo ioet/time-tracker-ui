2903f97103b6b87a11c9bf2ad80e1693421bcc9d
Cristina Guncay <macrisguncay@gmail.com>
2020-03-29 23:05:29 -0500

    49 fix menu items in sidebar

--- src/app/modules/shared/components/sidebar/sidebar.component.ts
+++ src/app/modules/shared/components/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ItemSidebar } from './models/item-sidebar.model';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  itemsSidebar: ItemSidebar[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getItemsSidebar();
   }
 
+  getItemsSidebar() {
+    this.itemsSidebar = [
+      {route: './time-clock', icon: 'far fa-clock', text: 'Time Clock' },
+      {route: './time-entries', icon: 'far fa-list-alt', text: 'Time Entries' },
+      {route: './reports', icon: 'fas fa-chart-pie', text: 'Reports' },
+      {route: './project-management', icon: 'far fa-folder-open', text: 'Projects' },
+      {route: './activities-management', icon: 'far fa-file-alt', text: 'Activities' }
+    ];
+  }
 }
