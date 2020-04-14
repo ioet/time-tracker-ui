63ceb24899c697e19c7b949e9f243609075425f4
Rene Enriquez <enriquezrene.ap@gmail.com>
2020-04-14 17:37:16 -0500

    fix: #130 missing client_id

--- scripts/populate-keys.sh
+++ scripts/populate-keys.sh
@@ -4,3 +4,4 @@
 echo 'export const AUTHORITY = "'$AUTHORITY'";' >> src/environments/keys.ts
 echo 'export const CLIENT_ID = "'$CLIENT_ID'";' >> src/environments/keys.ts
 echo 'export const SCOPES = ["'$SCOPES'"];' >> src/environments/keys.ts
+cat src/environments/keys.ts
