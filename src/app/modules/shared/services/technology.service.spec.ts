d614c2cbe31f119ec3f98719df91ed3358c0a375
Jorge Flores <jflores@ioet.com>
2020-04-14 10:29:32 -0500

    fix: #29 Fix test

--- src/app/modules/shared/services/technology.service.spec.ts
+++ src/app/modules/shared/services/technology.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Technology } from '../../shared/models';
 import { TechnologyService } from './technology.service';
-import { KEY } from '../../../../environments/environment';
+import { STACK_EXCHANGE_ID, STACK_EXCHANGE_ACCESS_TOKEN } from '../../../../environments/environment';
 
 describe('TechnologyService', () => {
   let service: TechnologyService;
@@ -37,7 +37,7 @@ describe('TechnologyService', () => {
       expect(technologyInResponse.items.length).toBe(technologyFoundSize);
     });
     const getTechnologiesRequest = httpMock.expectOne(
-      `${service.baseUrl}&inname=${technologyName}&site=stackoverflow&key=${KEY}`
+      `${service.baseUrl}&inname=${technologyName}&site=stackoverflow&key=${STACK_EXCHANGE_ID}&access_token=${STACK_EXCHANGE_ACCESS_TOKEN}`
     );
     expect(getTechnologiesRequest.request.method).toBe('GET');
     getTechnologiesRequest.flush(technologyList);
