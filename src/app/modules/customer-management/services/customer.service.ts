3c88af4cd7db4f86f715abbb20a12ee498a43aa1
Rene Enriquez <enriquezrene.ap@gmail.com>
2020-04-17 18:38:59 -0500

    fix: #141 remove unnecesary import

--- src/app/modules/customer-management/services/customer.service.ts
+++ src/app/modules/customer-management/services/customer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { environment } from './../../../../environments/environment';
 import { AzureAdB2CService } from 'src/app/modules/login/services/azure.ad.b2c.service';
