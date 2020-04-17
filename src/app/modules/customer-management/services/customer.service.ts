d4ff348c285ed9c8bdc86c7878aae7d7e21c5dd8
daros10 <daro.wla@hotmail.es>
2020-04-17 17:49:20 -0500

    fix: #87 get tenant_id from AzureServices

--- src/app/modules/customer-management/services/customer.service.ts
+++ src/app/modules/customer-management/services/customer.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { environment } from './../../../../environments/environment';
+import { AzureAdB2CService } from 'src/app/modules/login/services/azure.ad.b2c.service';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,13 +11,14 @@ import { Observable } from 'rxjs';
 export class CustomerService {
   baseUrl = `${environment.timeTrackerApiUrl}/customers`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private service: AzureAdB2CService) {}
 
   createCustomer(customerData): Observable<any> {
     const body = {
       ...customerData,
-      tenant_id: sessionStorage.getItem('tenant_id'),
+      tenant_id: this.service.getTenantId(),
     };
+    console.log(this.service.getTenantId());
     return this.http.post(this.baseUrl, body);
   }
 }
