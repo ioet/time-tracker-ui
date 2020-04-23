22e6a9915cae9a848e575a7043cef5d774236d98
Diego Tinitana <datinitanao@gmail.com>
2020-04-23 11:18:27 -0500

    fix: #147 Display correct username fix lints

--- src/app/modules/customer-management/components/projects/components/services/project.service.ts
+++ src/app/modules/customer-management/components/projects/components/services/project.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../../../../environments/environment';
 import { Project } from '../../../../../shared/models';
-import { AzureAdB2CService } from '../../../../../login/services/azure.ad.b2c.service';
 
 @Injectable({
   providedIn: 'root',
--- src/app/modules/customer-management/services/customer.service.ts
+++ src/app/modules/customer-management/services/customer.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 import { environment } from './../../../../environments/environment';
-import { AzureAdB2CService } from 'src/app/modules/login/services/azure.ad.b2c.service';
 
 @Injectable({
   providedIn: 'root',
