a637d555db1f66602f4b80b6293777b7a80eb1bd
Rene Enriquez <enriquezrene.ap@gmail.com>
2020-05-15 09:27:32 -0500

    fix: #243 creating a place where messages are hold

---
+++ src/app/modules/shared/messages.ts
@@ -0,0 +1,3 @@
+export const INFO_SAVED_SUCCESSFULLY = 'The data has been saved successfully';
+export const INFO_DELETE_SUCCESSFULLY = 'The data has been deleted successfully';
+export const UNEXPECTED_ERROR = 'An unexpected error happened, please try again later';
