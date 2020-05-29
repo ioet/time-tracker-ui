a53932d337f5c53270d94741acb5ac49dde0deac
Juan Gabriel Guzman <jguzman@ioet.com>
2020-05-29 15:56:47 -0500

    refactor: #204 Refactoring styles

--- src/app/modules/shared/components/input-label/input-label.component.ts
+++ src/app/modules/shared/components/input-label/input-label.component.ts
@@ -3,6 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'app-input-label',
   templateUrl: './input-label.component.html',
+  styleUrls: ['./input-label.component.scss']
 })
 export class InputLabelComponent {
   @Input()
