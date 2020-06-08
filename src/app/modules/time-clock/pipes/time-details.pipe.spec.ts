bafefe6e82b3287a56f82893b62c1481b321a77e
Rene Enriquez <enriquezrene.ap@gmail.com>
2020-06-08 13:11:38 -0500

    fix: #285 display current working time in time-clock screen

--- src/app/modules/time-clock/pipes/time-details.pipe.spec.ts
+++ src/app/modules/time-clock/pipes/time-details.pipe.spec.ts
@@ -1,3 +1,4 @@
+import { TimeDetails } from './../models/time.entry.summary';
 import { TimeDetailsPipe } from './time-details.pipe';
 
 describe('TimeDetailsPipe', () => {
@@ -9,5 +10,22 @@ describe('TimeDetailsPipe', () => {
   it('adds a 0 if has one number', () => {
     const pipe = new TimeDetailsPipe();
     expect(pipe.formatAsTwoDigit('5')).toBe('05');
+  });
+
+  it('returns the same data if number has 2 digits', () => {
+    const pipe = new TimeDetailsPipe();
+    expect(pipe.formatAsTwoDigit('15')).toBe('15');
+  });
+
+  it('formats hour in expected format', () => {
+    const timeDetails = { hours: '1', minutes: '9', seconds: '00'};
+    const pipe = new TimeDetailsPipe();
+    expect('01:09').toBe(pipe.transform(timeDetails));
+  });
+
+  it('returns --:-- when timeDetails is null', () => {
+    const timeDetails = null;
+    const pipe = new TimeDetailsPipe();
+    expect('--:--').toBe(pipe.transform(timeDetails));
   });
 });
