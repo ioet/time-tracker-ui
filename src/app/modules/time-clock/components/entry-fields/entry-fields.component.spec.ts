56ddc6b10675ac71f13df26d064531d22531f916
PaulRC-ioet <procha@ioet.com>
2020-10-21 15:38:18 -0500

    feat: edit start time in time-clock #233

--- src/app/modules/time-clock/components/entry-fields/entry-fields.component.spec.ts
+++ src/app/modules/time-clock/components/entry-fields/entry-fields.component.spec.ts
@@ -10,6 +10,9 @@ import {EntryFieldsComponent} from './entry-fields.component';
 import {ProjectState} from '../../../customer-management/components/projects/components/store/project.reducer';
 import {getCustomerProjects} from '../../../customer-management/components/projects/components/store/project.selectors';
 import { ActionsSubject } from '@ngrx/store';
+import { IndividualConfig, ToastrService } from 'ngx-toastr';
+import { formatDate } from '@angular/common';
+import * as moment from 'moment';
 
 describe('EntryFieldsComponent', () => {
   type Merged = TechnologyState & ProjectState;
@@ -20,6 +23,10 @@ describe('EntryFieldsComponent', () => {
   let mockProjectsSelector;
   let entryForm;
   const actionSub: ActionsSubject = new ActionsSubject();
+  const toastrServiceStub = {
+    error: (message?: string, title?: string, override?: Partial<IndividualConfig>) => { },
+    warning: (message?: string, title?: string, override?: Partial<IndividualConfig>) => { }
+  };
 
   const state = {
     projects: {
@@ -59,12 +66,18 @@ describe('EntryFieldsComponent', () => {
     project_id: 'project-id-15',
     description: 'description for active entry',
     uri: 'abc',
+    start_date : moment().format('YYYY-MM-DD'),
+    start_hour : moment().format('HH:mm:ss'),
   };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [EntryFieldsComponent],
-      providers: [provideMockStore({initialState: state}), { provide: ActionsSubject, useValue: actionSub }],
+      providers: [
+        provideMockStore({initialState: state}),
+        { provide: ActionsSubject, useValue: actionSub },
+        { provide: ToastrService, useValue: toastrServiceStub }
+      ],
       imports: [FormsModule, ReactiveFormsModule],
     }).compileComponents();
     store = TestBed.inject(MockStore);
@@ -97,8 +110,54 @@ describe('EntryFieldsComponent', () => {
 
     expect(component.entryForm.patchValue).toHaveBeenCalledTimes(1);
     expect(component.entryForm.patchValue).toHaveBeenCalledWith(
-      { description: entryDataForm.description, uri: entryDataForm.uri, activity_id: entryDataForm.activity_id });
+      {
+        description: entryDataForm.description,
+        uri: entryDataForm.uri,
+        activity_id: entryDataForm.activity_id,
+        start_hour:  formatDate(entry.start_date, 'HH:mm:ss', 'en')
+      }
+    );
     expect(component.selectedTechnologies).toEqual([]);
+  });
+
+  it('displays error message when the date selected is in the future', () => {
+    component.newData = entry;
+    component.activeEntry = entry ;
+    component.setDataToUpdate(entry);
+    spyOn(toastrServiceStub, 'error');
+
+    const hourInTheFuture = moment().add(1, 'hours').format('HH:mm:ss');
+    component.entryForm.patchValue({ start_hour : hourInTheFuture});
+    component.onUpdateStartHour();
+
+    expect(toastrServiceStub.error).toHaveBeenCalled();
+  });
+
+  it('If start hour is in the future, reset to initial start_date in form', () => {
+    component.newData = entry;
+    component.activeEntry = entry ;
+    component.setDataToUpdate(entry);
+
+    const hourInTheFuture = moment().add(1, 'hours').format('HH:mm:ss');
+    component.entryForm.patchValue({ start_hour : hourInTheFuture});
+
+    spyOn(component.entryForm, 'patchValue');
+    component.onUpdateStartHour();
+
+    expect(component.entryForm.patchValue).toHaveBeenCalledWith(
+      {
+        start_hour: component.newData.start_hour
+      }
+    );
+  });
+
+  it('when a start hour is updated, then dispatch UpdateActiveEntry', () => {
+    component.activeEntry = entry ;
+    component.setDataToUpdate(entry);
+    spyOn(store, 'dispatch');
+
+    component.onUpdateStartHour();
+    expect(store.dispatch).toHaveBeenCalled();
   });
 
   it('when a technology is added, then dispatch UpdateActiveEntry', () => {
