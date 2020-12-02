0e868042d18503616e5df6e9cea5aef89f931503
roberto <alexandro.ramr777@gmail.com>
2020-12-02 15:24:15 -0500

    TT-52 feat: add toggle flag

--- src/app/modules/shared/components/sidebar/sidebar.component.ts
+++ src/app/modules/shared/components/sidebar/sidebar.component.ts
@@ -4,6 +4,7 @@ import {ItemSidebar} from './models/item-sidebar.model';
 import {NavigationStart, Router} from '@angular/router';
 import {Observable} from 'rxjs';
 import {filter} from 'rxjs/operators';
+import { FeatureManagerService } from '../../feature-toggles/feature-toggle-manager.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -15,7 +16,11 @@ export class SidebarComponent implements OnInit {
   itemsSidebar: ItemSidebar[] = [];
   navStart;
 
-  constructor(private azureAdB2CService: AzureAdB2CService, private router: Router) {
+  constructor(
+    private azureAdB2CService: AzureAdB2CService,
+    private router: Router,
+    private featureManagerService : FeatureManagerService,
+  ) {
     this.navStart = this.router.events.pipe(
       filter(evt => evt instanceof NavigationStart)
     ) as Observable<NavigationStart>;
@@ -24,6 +29,7 @@ export class SidebarComponent implements OnInit {
   ngOnInit(): void {
     this.toggleSideBar();
     this.getSidebarItems();
+    this.toggleListTechnologies(this.itemsSidebar);
     this.highlightMenuOption(this.router.routerState.snapshot.url);
     this.navStart.subscribe(evt => {
       this.highlightMenuOption(evt.url);
@@ -46,7 +52,6 @@ export class SidebarComponent implements OnInit {
         {route: '/activities-management', icon: 'fas fa-file-alt', text: 'Activities', active: false},
         {route: '/customers-management', icon: 'fas fa-user', text: 'Customers', active: false},
         {route: '/users', icon: 'fas fa-user', text: 'Users', active: false},
-        {route: '/technology-report', icon: 'fas fa-user', text: 'Technology Report', active: false},
       ];
     } else {
       this.itemsSidebar = [
@@ -54,6 +59,23 @@ export class SidebarComponent implements OnInit {
         {route: '/time-entries', icon: 'fas fa-list-alt', text: 'Time Entries', active: false},
       ];
     }
+  }
+
+  toggleListTechnologies(itemsSidebar: ItemSidebar[]){
+    this.featureManagerService
+    .isToggleEnabledForUser('ui-list-technologies')
+    .subscribe((enabled) => {
+      console.log('enabled?:', enabled);
+      if (enabled === true){
+        const listTechnologiesItem = {
+          route: '/technology-report',
+          icon: 'fas fa-user',
+          text: 'Technology Report',
+          active: false
+        };
+        itemsSidebar.push(listTechnologiesItem);
+      };
+    });
   }
 
   highlightMenuOption(route) {
