48447259f068c60132c9762aa4315988458080e2
Guido Quezada <gqrott@gmail.com>
2020-12-09 14:23:29 -0500

    TT-15 fix: add test for login when cookie does not exist

--- src/app/guards/login-guard/login.guard.ts
+++ src/app/guards/login-guard/login.guard.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { AzureAdB2CService } from '../../modules/login/services/azure.ad.b2c.service';
-import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
 
-  constructor(private azureAdB2CService: AzureAdB2CService, private router: Router, private cookieService: CookieService) { }
+  constructor(private azureAdB2CService: AzureAdB2CService, private router: Router) { }
 
   canActivate() {
-    if (this.azureAdB2CService.isLogin() && this.cookieService.check('msal.idtoken')) {
+    if (this.azureAdB2CService.isLogin()) {
         this.azureAdB2CService.setCookies();
         return true;
     } else {
