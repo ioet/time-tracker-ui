8058078808e9b2c98bd96260bc159b5af556bb82
PaulRC-ioet <73141380+PaulRC-ioet@users.noreply.github.com>
2021-01-15 13:07:58 -0500

    TT-111 fix: Add a roles field in Users (#614)
    * TT-111 fix: Add a roles field in Users
    
    * TT-111 fix: add a test to check new model of users
    
    * TT-111 fix: add a new test for user roles

--- src/app/modules/users/components/users-list/users-list.component.spec.ts
+++ src/app/modules/users/components/users-list/users-list.component.spec.ts
@@ -14,7 +14,17 @@ describe('UsersListComponent', () => {
   const actionSub: ActionsSubject = new ActionsSubject();
 
   const state: UserState = {
-    data: [{ name: 'name', email: 'email', role: 'role', id: 'id', tenant_id: 'tenant id', deleted: 'delete' }],
+    data: [
+      {
+        name: 'name',
+        email: 'email',
+        role: null,
+        roles: ['admin', 'test'],
+        id: 'id',
+        tenant_id: 'tenant id',
+        deleted: 'delete',
+      },
+    ],
     isLoading: false,
     message: '',
   };
@@ -57,6 +67,51 @@ describe('UsersListComponent', () => {
     actionSubject.next(action);
 
     expect(component.users).toEqual(state.data);
+  });
+
+  it('on success load users, the data of roles should be an array and role null', () => {
+    const actionSubject = TestBed.inject(ActionsSubject) as ActionsSubject;
+    const action = {
+      type: UserActionTypes.LOAD_USERS_SUCCESS,
+      payload: state.data,
+    };
+
+    actionSubject.next(action);
+
+    component.users.map((user) => {
+      expect(user.role).toEqual(null);
+      expect(user.roles).toEqual(['admin', 'test']);
+    });
+  });
+
+  it('on success load users, the data of roles should be null and role a string', () => {
+    const actionSubject = TestBed.inject(ActionsSubject) as ActionsSubject;
+    const mockState: UserState = {
+      data: [
+        {
+          name: 'name',
+          email: 'email',
+          role: 'admin',
+          roles: null,
+          id: 'id',
+          tenant_id: 'tenant id',
+          deleted: 'delete',
+        },
+      ],
+      isLoading: false,
+      message: '',
+    };
+    const action = {
+      type: UserActionTypes.LOAD_USERS_SUCCESS,
+      payload: mockState.data,
+    };
+
+    actionSubject.next(action);
+
+    component.users.map((user) => {
+      expect(user.role).toEqual('admin');
+      expect(user.roles).toEqual(null);
+    });
   });
 
   it('on success load users, the datatable should be reloaded', async () => {
