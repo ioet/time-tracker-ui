29e1b27dab6430b13b0c1f050edc8a3f663662a8
thegreatyamori <tod.jerson.morocho@gmail.com>
2021-03-19 10:45:43 -0500

    test: TT-155 deleted two extra lines in sidebar

--- src/app/modules/shared/components/sidebar/sidebar.component.ts
+++ src/app/modules/shared/components/sidebar/sidebar.component.ts
@@ -5,7 +5,6 @@ import {NavigationStart, Router} from '@angular/router';
 import {Observable} from 'rxjs';
 import {filter} from 'rxjs/operators';
 import { FeatureManagerService } from '../../feature-toggles/feature-toggle-manager.service';
-import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-sidebar',
@@ -21,8 +20,6 @@ export class SidebarComponent implements OnInit {
     private azureAdB2CService: AzureAdB2CService,
     private router: Router,
     private featureManagerService: FeatureManagerService,
-    private store: Store,
-    private bc2azure: AzureAdB2CService,
   ) {
     this.navStart = this.router.events.pipe(
       filter(evt => evt instanceof NavigationStart)
@@ -37,9 +34,6 @@ export class SidebarComponent implements OnInit {
     this.navStart.subscribe(evt => {
       this.highlightMenuOption(evt.url);
     });
-
-    console.log(this.bc2azure.getUserId());
-
   }
 
   toggleSideBar() {
