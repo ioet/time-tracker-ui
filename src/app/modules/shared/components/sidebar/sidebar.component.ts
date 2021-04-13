29bdef30e75fab36ed9de96c3edceb630de078dc
Jerson Morocho <tod.jerson.morocho@gmail.com>
2021-04-13 10:48:58 -0500

    feat: TT-191 Validate the new flow for permission management (#661)
    * feat: TT-191 add FT switch to sidebar.component
    
    * style: TT-191 styling sidebar code

--- src/app/modules/shared/components/sidebar/sidebar.component.ts
+++ src/app/modules/shared/components/sidebar/sidebar.component.ts
@@ -1,39 +1,46 @@
-import {AzureAdB2CService} from 'src/app/modules/login/services/azure.ad.b2c.service';
-import {Component, OnInit} from '@angular/core';
-import {ItemSidebar} from './models/item-sidebar.model';
-import {NavigationStart, Router} from '@angular/router';
-import {Observable} from 'rxjs';
-import {filter} from 'rxjs/operators';
+import { AzureAdB2CService } from 'src/app/modules/login/services/azure.ad.b2c.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ItemSidebar } from './models/item-sidebar.model';
+import { NavigationStart, Router } from '@angular/router';
+import { Observable, of, Subscription } from 'rxjs';
+import { filter, map, mergeMap } from 'rxjs/operators';
 import { FeatureManagerService } from '../../feature-toggles/feature-toggle-manager.service';
+import { UserInfoService } from 'src/app/modules/user/services/user-info.service';
+import { FeatureSwitchGroupService } from '../../feature-toggles/switch-group/feature-switch-group.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent implements OnInit {
-
+export class SidebarComponent implements OnInit, OnDestroy {
   itemsSidebar: ItemSidebar[] = [];
   navStart;
+  FTSwitchGroup$: Subscription;
 
   constructor(
-    private azureAdB2CService: AzureAdB2CService,
     private router: Router,
+    private userInfoService: UserInfoService,
+    private azureAdB2CService: AzureAdB2CService,
     private featureManagerService: FeatureManagerService,
+    private featureSwitchGroup: FeatureSwitchGroupService
   ) {
     this.navStart = this.router.events.pipe(
-      filter(evt => evt instanceof NavigationStart)
+      filter((evt) => evt instanceof NavigationStart)
     ) as Observable<NavigationStart>;
   }
 
   ngOnInit(): void {
     this.toggleSideBar();
-    this.getSidebarItems();
+    this.FTSwitchGroup$ = this.getSidebarItems().subscribe();
     this.toggleListTechnologies(this.itemsSidebar);
     this.highlightMenuOption(this.router.routerState.snapshot.url);
-    this.navStart.subscribe(evt => {
+    this.navStart.subscribe((evt) => {
       this.highlightMenuOption(evt.url);
     });
+  }
+  ngOnDestroy(): void {
+    this.FTSwitchGroup$.unsubscribe();
   }
 
   toggleSideBar() {
@@ -43,42 +50,51 @@ export class SidebarComponent implements OnInit {
     });
   }
 
-  getSidebarItems() {
-    if (this.azureAdB2CService.isAdmin()) {
-      this.itemsSidebar = [
-        {route: '/time-clock', icon: 'fas fa-clock', text: 'Time Clock', active: false},
-        {route: '/time-entries', icon: 'fas fa-list-alt', text: 'Time Entries', active: false},
-        {route: '/reports', icon: 'fas fa-chart-pie', text: 'Reports', active: false},
-        {route: '/activities-management', icon: 'fas fa-file-alt', text: 'Activities', active: false},
-        {route: '/customers-management', icon: 'fas fa-user', text: 'Customers', active: false},
-        {route: '/users', icon: 'fas fa-user', text: 'Users', active: false},
-      ];
-    } else {
-      this.itemsSidebar = [
-        {route: '/time-clock', icon: 'fas fa-clock', text: 'Time Clock', active: false},
-        {route: '/time-entries', icon: 'fas fa-list-alt', text: 'Time Entries', active: false},
-      ];
-    }
+  getSidebarItems(): Observable<void> {
+    const isAdminBasedInRole = of(this.azureAdB2CService.isAdmin());
+    const isAdminBasedInGroup = this.userInfoService.isAdmin();
+    return this.featureSwitchGroup.isActivated().pipe(
+      mergeMap((enabled) => {
+        return enabled ? isAdminBasedInGroup : isAdminBasedInRole;
+      }),
+      map((isAdmin) => {
+        if (isAdmin) {
+          this.itemsSidebar = [
+            { route: '/time-clock', icon: 'fas fa-clock', text: 'Time Clock', active: false },
+            { route: '/time-entries', icon: 'fas fa-list-alt', text: 'Time Entries', active: false },
+            { route: '/reports', icon: 'fas fa-chart-pie', text: 'Reports', active: false },
+            { route: '/activities-management', icon: 'fas fa-file-alt', text: 'Activities', active: false },
+            { route: '/customers-management', icon: 'fas fa-user', text: 'Customers', active: false },
+            { route: '/users', icon: 'fas fa-user', text: 'Users', active: false },
+          ];
+        } else {
+          this.itemsSidebar = [
+            { route: '/time-clock', icon: 'fas fa-clock', text: 'Time Clock', active: false },
+            { route: '/time-entries', icon: 'fas fa-list-alt', text: 'Time Entries', active: false },
+          ];
+        }
+      })
+    );
   }
 
-  toggleListTechnologies(itemsSidebar: ItemSidebar[]){
+  toggleListTechnologies(itemsSidebar: ItemSidebar[]) {
     this.featureManagerService
-    .isToggleEnabledForUser('ui-list-technologies')
-    .subscribe((enabled) => {
-      if (enabled === true){
-        const listTechnologiesItem = {
-          route: '/technology-report',
-          icon: 'fas fa-user',
-          text: 'Technology Report',
-          active: false
-        };
-        itemsSidebar.push(listTechnologiesItem);
-      }
-    });
+      .isToggleEnabledForUser('ui-list-technologies')
+      .subscribe((enabled) => {
+        if (enabled === true) {
+          const listTechnologiesItem = {
+            route: '/technology-report',
+            icon: 'fas fa-user',
+            text: 'Technology Report',
+            active: false,
+          };
+          itemsSidebar.push(listTechnologiesItem);
+        }
+      });
   }
 
   highlightMenuOption(route) {
-    this.itemsSidebar.map(item => item.active = false);
-    this.itemsSidebar.filter(item => item.route === route).map(item => item.active = true);
+    this.itemsSidebar.map((item) => (item.active = false));
+    this.itemsSidebar.filter((item) => item.route === route).map((item) => (item.active = true));
   }
 }
