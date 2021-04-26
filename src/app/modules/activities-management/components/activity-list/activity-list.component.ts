17d5acd7fce9cef1c1e96a887bdda9f870f577e6
thegreatyamori <tod.jerson.morocho@gmail.com>
2021-04-26 10:49:54 -0500

    feat: TT-208 remove delete button & assign openmodal to switch

--- src/app/modules/activities-management/components/activity-list/activity-list.component.ts
+++ src/app/modules/activities-management/components/activity-list/activity-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { getIsLoading } from 'src/app/modules/activities-management/store/activity-management.selectors';
 import { Activity } from '../../../shared/models';
@@ -8,22 +8,21 @@ import { allActivities } from '../../store';
 import { DeleteActivity, LoadActivities, SetActivityToEdit } from './../../store/activity-management.actions';
 import { ActivityState } from './../../store/activity-management.reducers';
 
-
 @Component({
   selector: 'app-activity-list',
   templateUrl: './activity-list.component.html',
   styleUrls: ['./activity-list.component.scss'],
 })
 export class ActivityListComponent implements OnInit {
+  constructor(private store: Store<ActivityState>) {
+    this.isLoading$ = store.pipe(delay(0), select(getIsLoading));
+  }
   activities: Activity[] = [];
   showModal = false;
   activityToDelete: Activity;
   message: string;
   idToDelete: string;
   isLoading$: Observable<boolean>;
-  constructor(private store: Store<ActivityState>) {
-    this.isLoading$ = store.pipe(delay(0), select(getIsLoading));
-  }
 
   ngOnInit() {
     this.store.dispatch(new LoadActivities());
@@ -35,8 +34,9 @@ export class ActivityListComponent implements OnInit {
   }
 
   deleteActivity() {
-    this.store.dispatch(new DeleteActivity(this.idToDelete));
-    this.showModal = true;
+    // this.store.dispatch(new DeleteActivity(this.idToDelete));
+    console.log('despachado el evento');
+    this.showModal = false;
   }
 
   updateActivity(activityId: string) {
@@ -45,15 +45,26 @@ export class ActivityListComponent implements OnInit {
 
   openModal(item: Activity) {
     this.idToDelete = item.id;
-    this.message = `Are you sure you want to delete ${item.name}?`;
+    this.message = `Are you sure you want to archive activity ${item.name}?`;
     this.showModal = true;
+    console.log(`Despliegue del modal para id: ${item.id}`);
   }
 
-  switchStatus(state: string): void {
-    // this.store.dispatch(
-    //   user.groups.includes(groupName)
-    //     ? new RemoveUserFromGroup(user.id, groupName)
-    //     : new AddUserToGroup(user.id, groupName)
-    // );
+  switchStatus(evt: boolean, item: Activity): void {
+    if (!evt) {
+      // FIXME: Si el usuario selecciona cancelar, se produce la animación de cambio
+      // del switch
+      this.openModal(item);
+    } else {
+      this.showModal = false;
+      // TODO: dispatch para actualizar la operacion
+      // this.store.dispatch(new SetToTrueActivity());
+    }
+  }
+
+  OnSwitch(item: Activity): Observable<boolean> {
+    // FIXME: OnBeforeChange debería ser un evento que traiga el estado del switch
+    // para poder realizar una acción antes de que el estado cambie.
+    return of(true);
   }
 }
