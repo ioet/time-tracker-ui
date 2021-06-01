76d763f5e80de1cb207c1e1d826385388cdff8e3
Kevin Lopez <kevinjeff04@gmail.com>
2021-06-01 18:49:15 -0500

    fix: TT-258 modificate atribute star_date to view time in

--- src/app/modules/time-entries/pages/time-entries.component.spec.ts
+++ src/app/modules/time-entries/pages/time-entries.component.spec.ts
@@ -381,7 +381,7 @@ describe('TimeEntriesComponent', () => {
       uri: 'http://testing.is.fun',
       activity_id: 'sss',
       project_id: 'id',
-      start_date: component.canMarkEntryAsWIP ? new Date() : new Date(new Date().setHours(0, 0, 0, 0)),
+      start_date: new Date(),
       end_date: new Date(new Date().setHours(0, 0, 0, 0))
     };
     state.timeEntriesDataSource.data = [lastEntry];
