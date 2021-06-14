1add6129835e7970da34f5d37a04eb37cf236ff7
Kevin Jefferson Lopez De la O <kevjlope@espol.edu.ec>
2021-06-14 10:47:47 -0500

    fix: TT-262 disable buttons futures in time entries and disable input date futures  (#697)
    * fix: TT-262 disable buttoms futures in time entries and disable input-date futures in reports, test of method monthEnable
    
    * fix: TT-262 disable buttoms futures in time entries and disable input-date futures in reports, test of method monthEnable
    
    * fix: TT-262 refactor code based on PR reviews

--- src/app/modules/shared/components/month-picker/month-picker.component.spec.ts
+++ src/app/modules/shared/components/month-picker/month-picker.component.spec.ts
@@ -7,7 +7,6 @@ import { MonthPickerComponent } from './month-picker.component';
 describe('MonthPickerComponent', () => {
   let component: MonthPickerComponent;
   let fixture: ComponentFixture<MonthPickerComponent>;
-
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MonthPickerComponent ]
@@ -67,6 +66,11 @@ describe('MonthPickerComponent', () => {
     component.selectMonth(monthSelect);
 
     expect(component.selectDate).toHaveBeenCalledWith(monthSelect, yearSelect);
+  });
+
+  it('monthEnable sets disabled to true on futures months', () => {
+    const monthFuture =  component.monthCurrent + 1;
+    expect(component.monthEnable(monthFuture)).toBeTrue();
   });
 
 });
