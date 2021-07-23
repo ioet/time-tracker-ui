981cfb812ec50b1df9d58149113958429682e492
Santiago Pozo Ruiz <38196801+DrFreud1@users.noreply.github.com>
2021-07-23 14:37:25 -0500

    Update details-fields.component.ts

--- src/app/modules/shared/components/details-fields/details-fields.component.ts
+++ src/app/modules/shared/components/details-fields/details-fields.component.ts
@@ -269,7 +269,6 @@ export class DetailsFieldsComponent implements OnChanges, OnInit {
     const updatedHour = this.entryForm.value[hour];
     const updatedDate = new Date(`${entryFormDate}T${updatedHour.trim()}`).toISOString();
     const initialDate = get(this.entryToEdit, date, updatedDate);
-    const initialHour = formatDate(get(this.entryToEdit, date, updatedDate), 'HH:mm', 'en');
     const dateHasNotChanged = (initialDate === updatedDate);
     const result = dateHasNotChanged ? initialDate : updatedDate;
     return result;
