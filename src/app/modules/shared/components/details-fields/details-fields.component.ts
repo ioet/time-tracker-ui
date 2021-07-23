d12ce958e974b4407ef9f77a53eff5e4a0362bd8
Santiago Pozo Ruiz <38196801+DrFreud1@users.noreply.github.com>
2021-07-23 14:42:12 -0500

    Tt 289 cannot edit the date of an entry (#708)
    * fix: TT-289 added an extra comparison to make sure the update date is being saved
    
    * fix: TT-289 remove non-relevant anymore test cases, after fist changes
    
    * Update details-fields.component.ts
    
    * Update details-fields.component.ts

--- src/app/modules/shared/components/details-fields/details-fields.component.ts
+++ src/app/modules/shared/components/details-fields/details-fields.component.ts
@@ -269,8 +269,7 @@ export class DetailsFieldsComponent implements OnChanges, OnInit {
     const updatedHour = this.entryForm.value[hour];
     const updatedDate = new Date(`${entryFormDate}T${updatedHour.trim()}`).toISOString();
     const initialDate = get(this.entryToEdit, date, updatedDate);
-    const initialHour = formatDate(get(this.entryToEdit, date, updatedDate), 'HH:mm', 'en');
-    const dateHasNotChanged = updatedHour === initialHour;
+    const dateHasNotChanged = (initialDate === updatedDate);
     const result = dateHasNotChanged ? initialDate : updatedDate;
     return result;
   }
