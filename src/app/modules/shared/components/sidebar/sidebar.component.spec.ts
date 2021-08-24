e4642d98920432c8fe7e652c5fcfe1e03aad0531
DrFreud1 <santiagopozoruiz@gmail.com>
2021-08-24 15:36:37 -0500

    code-smell: TT-316 removal of an additional unnecessary import

--- src/app/modules/shared/components/sidebar/sidebar.component.spec.ts
+++ src/app/modules/shared/components/sidebar/sidebar.component.spec.ts
@@ -5,7 +5,6 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { Router, Routes } from '@angular/router';
 import { TimeClockComponent } from '../../../time-clock/pages/time-clock.component';
 import { of } from 'rxjs';
-import { FeatureManagerService } from '../../feature-toggles/feature-toggle-manager.service';
 import { UserInfoService } from 'src/app/modules/user/services/user-info.service';
 
 describe('SidebarComponent', () => {
