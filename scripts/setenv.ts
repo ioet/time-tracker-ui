2dafbcdffabb091381f13f043987947312164aed
Michael Villarruel <alejovillarruel@gmail.com>
2022-02-11 11:44:31 -0500

    TT 511 containerize time tracker UI (#810)
    * feat: TT-511 containerize time tracker ui
    
    * feat: TT-511 environment variables and tests setup
    
    * fix: TT-511 add missing environment variables
    
    * fix: TT-511 update .dockerignore
    
    * refactor: TT-511 solve comments
    
    * feat: TT-511 multi-stage builds in dockerfile and add user to run commands inside docker
    
    * feat: TT-511 create build and docker image for production
    
    * refactor: TT-511 solve comments
    
    * fix: TT-511 changes in makefile to solve comments
    
    * fix: TT-511 fix error when build to production

---
+++ scripts/setenv.ts
@@ -0,0 +1,34 @@
+const { writeFile } = require('fs');
+require('dotenv').config();
+
+const pathJs = `./src/environments/keys.ts`
+const contentKeys = 
+`export const AUTHORITY = '${process.env.AUTHORITY}';
+export const CLIENT_ID = '${process.env.CLIENT_ID}';
+export const SCOPES = ['${process.env.SCOPES}'];
+export const STACK_EXCHANGE_ID = '${process.env.STACK_EXCHANGE_ID}';
+export const STACK_EXCHANGE_ACCESS_TOKEN = '${process.env.STACK_EXCHANGE_ACCESS_TOKEN}';
+export const AZURE_APP_CONFIGURATION_CONNECTION_STRING = '${process.env.AZURE_APP_CONFIGURATION_CONNECTION_STRING}';
+`;
+
+writeFile(pathJs, contentKeys, function (err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(`Wrote variables to ${pathJs}`);
+});
+
+const pathJson = `./src/environments/.keys.json`
+const contentKeysJson = 
+`{
+    "authority": "${process.env.AUTHORITY_JSON}",
+    "client_id": "${process.env.CLIENT_ID_JSON}",
+    "scopes": ["${process.env.SCOPES_JSON}"]
+}`;
+
+writeFile(pathJson, contentKeysJson, function (err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(`Wrote variables to ${pathJson}`);
+});
