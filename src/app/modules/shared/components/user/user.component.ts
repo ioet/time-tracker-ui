470bd995ad1d8a9aaae91920667b39aada2dc8d5
kjduy <k.vn.jdh@gmail.com>
2022-03-14 16:45:20 -0500

    feat: TT-577 get profile pic from Google account

--- src/app/modules/shared/components/user/user.component.ts
+++ src/app/modules/shared/components/user/user.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { AzureAdB2CService } from '../../../login/services/azure.ad.b2c.service';
 import { LoginService } from '../../../login/services/login.service';
+import { SocialUser } from 'angularx-social-login';
+import { SocialAuthService } from 'angularx-social-login';
 
 @Component({
   selector: 'app-user',
@@ -9,25 +11,37 @@ import { LoginService } from '../../../login/services/login.service';
   styleUrls: ['./user.component.scss'],
 })
 export class UserComponent implements OnInit {
+  user: SocialUser;
+  userPhoto: string;
   userName: string;
   userEmail: string;
   isProduction = environment.production;
 
-  constructor(private azureAdB2CService: AzureAdB2CService, private loginService: LoginService) {}
+  constructor(
+    private azureAdB2CService: AzureAdB2CService,
+    private loginService: LoginService,
+    private authService: SocialAuthService,
+  ) { }
 
   ngOnInit(): void {
-    if (this.isProduction){
+    if (this.isProduction) {
       if (this.azureAdB2CService.isLogin()) {
         this.userName = this.azureAdB2CService.getName();
         this.userEmail = this.azureAdB2CService.getUserEmail();
         this.azureAdB2CService.setTenantId();
       }
-    }else{
+    } else {
       if (this.loginService.isLogin()) {
         this.userName = this.loginService.getName();
         this.userEmail = this.loginService.getUserEmail();
         this.azureAdB2CService.setTenantId();
       }
     }
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.userPhoto = user.photoUrl;
+      localStorage.setItem('googleUserPhoto', this.userPhoto);
+    });
+    this.userPhoto = localStorage.getItem('googleUserPhoto');
   }
 }
