75e909a1c0508091ea663239b92eaabe4816e18e
Jamil Torres <42499083+jatobrun@users.noreply.github.com>
2022-03-22 08:59:27 -0500

    feat: TT-509 Use terraform to provision ui infraestructure (#819)
    * feat: TT-509 Use terraform to provision ui infraestructure
    
    * feat: TT-580 Missing placeholders (#820)
    
    * feat: TT-580 Missing placeholders
    
    * feat: TT-580 add placeholders in ticket uri and description text inputs on the time clock page
    
    Co-authored-by: Luis Moyon <amoyon@hotmail.com>
    
    * chore(release): 1.66.0 [skip ci]nn
    
    * TT-xx: Fix make publish tasks (#822)
    
    * fix: TT-509 expose port 8080 in docker images
    
    * fix: TT-509 change app to port 80
    
    * feat: TT-578 improve the style of the form element (#823)
    
    * feat: TT-578 improve the style of the form element
    
    * chore(release): 1.67.0 [skip ci]nn
    
    * TT-582 Improve the summary section of the time-clock page and the clock out button (#824)
    
    * style: TT-582 improve the summary section with a new row with information of actual clock in register and clock out button
    
    * fix: TT-582 add missing unit test to confirm that clockout function in TimeEntriesSummaryComponent emits an event
    
    * style: TT-582 add a border radius to current elapsed time box
    
    Co-authored-by: David Cadena <david.cadena@hotmail.com>
    
    * fix: TT-509 including empty line in all files
    
    * fix: TT-509 Change the dockerfile to use root user and not app user
    
    * feat: TT-576 Improve grids visual presentation and function (#826)
    
    * feat: TT-576 Improve grids visual presentation and function
    
    * chore(release): 1.68.0 [skip ci]nn
    
    * feat: TT-577 get profile pic from Google account (#825)
    
    * feat: TT-577 get profile pic from Google account
    
    * feat: TT-577 get profile pic from Google account
    
    * feat: TT-577 get profile pic from Google account
    
    Co-authored-by: alejandra-ponce <alejandra.ponce@ioet.com>
    
    * chore(release): 1.69.0 [skip ci]nn
    
    * Revert "feat: TT-577 get profile pic from Google account (#825)" (#829)
    
    This reverts commit 3bee7e380bb7ed5f979dcd08dd29d0afed1f6fc0.
    
    Co-authored-by: Carlos Carvajal <56209390+cxcarvaj@users.noreply.github.com>
    
    * chore(release): 1.69.1 [skip ci]nn
    
    * style: TT-579 Buttons and searchbar adjusted to the app style (#827)
    
    * fix: TT-590 unable to pass ci test (#831)
    
    * chore(release): 1.69.2 [skip ci]nn
    
    * Tt 582 improve the summary section of the time clock page QA (#830)
    
    * style: TT-582 improve the summary section with a new row with information of actual clock in register and clock out button
    
    * fix: TT-582 add missing unit test to confirm that clockout function in TimeEntriesSummaryComponent emits an event
    
    * style: TT-582 add a border radius to current elapsed time box
    
    * style: TT-582 improved the clocked in hour text and current time box
    
    * style: TT-582 improved the clocked in hour text and current time box
    
    * style: TT-582 removed whitespace
    
    Co-authored-by: Luis Moyón <amoyon@hotmail.com>
    
    * fix: TT-509 pr comments
    
    * fix: TT-509 using remote state to load variables
    
    * Update variables.tf
    
    * delete prefix docker
    
    * delete prefix docker in locals block
    
    * fix: Update the values of the terraform state output
    
    Co-authored-by: alejandra-ponce <101274839+alejandra-ponce@users.noreply.github.com>
    Co-authored-by: Luis Moyon <amoyon@hotmail.com>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>
    Co-authored-by: Fausto Castaneda <info@faustocv.org>
    Co-authored-by: iHackN3WTON <92641464+iHackN3WTON@users.noreply.github.com>
    Co-authored-by: LuisMS7 <31932535+LuisMS7@users.noreply.github.com>
    Co-authored-by: David Cadena <david.cadena@hotmail.com>
    Co-authored-by: Kevin Joseph Duy Hurtado <44406663+kjduy@users.noreply.github.com>
    Co-authored-by: alejandra-ponce <alejandra.ponce@ioet.com>
    Co-authored-by: Carlos Carvajal <56209390+cxcarvaj@users.noreply.github.com>
    Co-authored-by: MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
    Co-authored-by: FreddyJR1995 <freddy.valverde@epn.edu.ec>

---
+++ infrastructure/variables.tf
@@ -0,0 +1,17 @@
+variable "image_tag" {
+  type        = string
+  description = "Specifies the docker image tag that is stored in a private container registry like ACR (Azure Container Registry)."
+  sensitive   = true
+}
+
+variable "service_plan_size" {
+  default     = "S1"
+  type        = string
+  description = "Specifies the size of the service plan. This variable format is: Tier (letter) + Size (number). Size could be: 1 = Small (1 Core 1.75GB RAM), 2 = Medium (2 Core 3.5 GB RAM), 3 = Large (4 Core 7GB RAM)"
+}
+
+variable "service_plan_tier" {
+  default     = "Standard"
+  type        = string
+  description = "Specifies the tier of the service plan. Tier is the pricing plan of the service plan resource."
+}
