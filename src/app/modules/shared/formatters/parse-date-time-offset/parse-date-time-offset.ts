29f5c0734bcd0163057c872dc3d1467f816a0e7f
Sergio Bateca <sbtk87@hotmail.com>
2022-04-04 11:01:36 -0500

    Implement test to ParseDateTimeOffset

--- src/app/modules/shared/formatters/parse-date-time-offset/parse-date-time-offset.ts
+++ src/app/modules/shared/formatters/parse-date-time-offset/parse-date-time-offset.ts
@@ -2,7 +2,7 @@ import * as moment from 'moment';
 
 export class ParseDateTimeOffset  {
   
-  parseDateTimeOffset(date:string, offset): string{
+  parseDateTimeOffset(date:string, offset): string{    
     return moment.utc(date).utcOffset(-1*offset).format("HH:mm");
   }
 }
--- src/app/modules/time-clock/services/entry.service.ts
+++ src/app/modules/time-clock/services/entry.service.ts
@@ -75,7 +75,8 @@ export class EntryService {
           start_date: this.datePipe.transform(range.start_date, EntryService.TIME_ENTRIES_DATE_TIME_FORMAT),
           end_date: this.datePipe.transform(range.end_date, EntryService.TIME_ENTRIES_DATE_TIME_FORMAT),
           user_id: userId,
-          limit: `${MAX_NUMBER_OF_ENTRIES_FOR_REPORTS}`
+          limit: `${MAX_NUMBER_OF_ENTRIES_FOR_REPORTS}`,
+          timezone_offset : new Date().getTimezoneOffset().toString(),
         }
       }
     );
