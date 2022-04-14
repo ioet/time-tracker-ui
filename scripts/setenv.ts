a627255b3f8d97dac2ef767b5fe430a8390ba946
Erick Almeida <erick.almeida@ioet.com>
2022-04-14 13:08:55 -0500

    fix: TT-610 Fix pipeline - read from OS env variable in .ts

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -1,14 +1,13 @@
 const { writeFile } = require('fs');
-require('dotenv').config();
 
 const pathJs = `./src/environments/keys.ts`
 const contentKeys =
-`export const AUTHORITY = '${process.env.AUTHORITY}';
-export const CLIENT_ID = '${process.env.CLIENT_ID}';
-export const SCOPES = ['${process.env.SCOPES}'];
-export const STACK_EXCHANGE_ID = '${process.env.STACK_EXCHANGE_ID}';
-export const STACK_EXCHANGE_ACCESS_TOKEN = '${process.env.STACK_EXCHANGE_ACCESS_TOKEN}';
-export const AZURE_APP_CONFIGURATION_CONNECTION_STRING = '${process.env.AZURE_APP_CONFIGURATION_CONNECTION_STRING}';
+`export const AUTHORITY = '${process.env["AUTHORITY"]}';
+export const CLIENT_ID = '${process.env["CLIENT_ID"]}';
+export const SCOPES = ['${process.env["SCOPES"]}'];
+export const STACK_EXCHANGE_ID = '${process.env["STACK_EXCHANGE_ID"]}';
+export const STACK_EXCHANGE_ACCESS_TOKEN = '${process.env["STACK_EXCHANGE_ACCESS_TOKEN"]}';
+export const AZURE_APP_CONFIGURATION_CONNECTION_STRING = '${process.env["AZURE_APP_CONFIGURATION_CONNECTION_STRING"]}';
 `;
 
 writeFile(pathJs, contentKeys, function (err) {
