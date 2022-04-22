50b2803abd868869ffef027b55a3acc1a41af099
Sergio Bateca <sbtk87@hotmail.com>
2022-04-22 11:25:33 -0500

    Fix invalid label to in progress label

--- src/app/modules/shared/formatters/parse-date-time-offset/parse-date-time-offset.ts
+++ src/app/modules/shared/formatters/parse-date-time-offset/parse-date-time-offset.ts
@@ -2,7 +2,8 @@ import * as moment from 'moment';
 
 export class ParseDateTimeOffset  {
   
-  parseDateTimeOffset(date:string, offset): string{    
+  parseDateTimeOffset(date:string, offset): string{
+    if(date == null || date == undefined || date == '') return 'in progress';    
     return moment.utc(date).utcOffset(-1*offset).format("HH:mm");
   }
 }
