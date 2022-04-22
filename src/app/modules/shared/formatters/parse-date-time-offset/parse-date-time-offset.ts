89d1e6a78591ca7eb1cbb3bf4113625ee523bd4d
Sergio Bateca <sbtk87@hotmail.com>
2022-04-22 09:32:13 -0500

    Adjust default text when end-date value is null

--- src/app/modules/shared/formatters/parse-date-time-offset/parse-date-time-offset.ts
+++ src/app/modules/shared/formatters/parse-date-time-offset/parse-date-time-offset.ts
@@ -2,7 +2,8 @@ import * as moment from 'moment';
 
 export class ParseDateTimeOffset  {
   
-  parseDateTimeOffset(date:string, offset): string{    
+  parseDateTimeOffset(date:string, offset): string{
+    if(date == null) return 'In process';
     return moment.utc(date).utcOffset(-1*offset).format("HH:mm");
   }
 }
