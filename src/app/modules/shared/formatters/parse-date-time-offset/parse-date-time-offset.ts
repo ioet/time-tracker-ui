575aee51deae939d5e63d0829f77c3397b83d8db
Sergio Bateca <sbtk87@hotmail.com>
2022-04-25 09:52:36 -0500

    Adjust the text with the capital first letter

--- src/app/modules/shared/formatters/parse-date-time-offset/parse-date-time-offset.ts
+++ src/app/modules/shared/formatters/parse-date-time-offset/parse-date-time-offset.ts
@@ -3,7 +3,7 @@ import * as moment from 'moment';
 export class ParseDateTimeOffset  {
   
   parseDateTimeOffset(date:string, offset): string{
-    if(date == null || date == undefined || date == '') return 'in progress';    
+    if(date == null || date == undefined || date == '') return 'In progress';    
     return moment.utc(date).utcOffset(-1*offset).format("HH:mm");
   }
 }
