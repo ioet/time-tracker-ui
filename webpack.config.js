74b556a649f91a7aba7f86a640ca8aebca1e817e
nicolsss <56645701+nicolsss@users.noreply.github.com>
2022-05-17 12:19:12 -0500

    TT 630 Remove keys.ts to set env variables (#864)
    * Removing use of keys.ts to store evironment variables
    
    * fix: update prod environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
+const webpack = require('webpack')
 const { addTailwindPlugin } = require("@ngneat/tailwind");
 const tailwindConfig = require("./tailwind.config.js");
-
 module.exports = (config) => {
+  const config_ = {
+    ...config,
+    plugins : [
+      ...config.plugins,
+      new webpack.DefinePlugin({
+        'process.env.AUTHORITY': JSON.stringify(process.env["AUTHORITY"]),
+        'process.env.CLIENT_ID':JSON.stringify(process.env["CLIENT_ID"]),
+        'process.env.SCOPES':JSON.stringify(process.env["SCOPES"]),
+        'process.env.STACK_EXCHANGE_ID':JSON.stringify(process.env["STACK_EXCHANGE_ID"]),
+        'process.env.STACK_EXCHANGE_ACCESS_TOKEN':JSON.stringify(process.env["STACK_EXCHANGE_ACCESS_TOKEN"]),
+        'process.env.AZURE_APP_CONFIGURATION_CONNECTION_STRING':JSON.stringify(process.env["AZURE_APP_CONFIGURATION_CONNECTION_STRING"])
+    })
+  ]
+  }
   addTailwindPlugin({
-    webpackConfig: config,
+    webpackConfig: config_,
     tailwindConfig,
     patchComponentsStyles: true
   });
-  return config;
+  return config_;
 };
