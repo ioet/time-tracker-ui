c2ab1fa9d62047e940f0c01854da12f7e1282811
Rodolfo <105673045+rodolfoIOET@users.noreply.github.com>
2022-08-19 18:24:33 -0500

    TTA-139 launch UI app service

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -41,21 +41,21 @@ locals {
   image_name              = "timetracker_ui"
 }
 
-# module "ui" {
-#   #source = "../../infra-terraform-modules/azure-app-service"
-#   source                   = "git@github.com:ioet/infra-terraform-modules.git//azure-app-service?ref=tags/v0.0.13"
-#   app_service_name         = "${local.service_name}-ui"
-#   create_app_service_plan  = local.create_app_service_plan
-#   docker_image_name        = "${local.image_name}:${var.image_tag}"
-#   docker_image_namespace   = data.terraform_remote_state.service.outputs.container_registry_login_server
-#   docker_registry_password = data.terraform_remote_state.service.outputs.container_registry_admin_password
-#   docker_registry_url      = data.terraform_remote_state.service.outputs.container_registry_login_server
-#   docker_registry_username = data.terraform_remote_state.service.outputs.container_registry_admin_username
-#   location                 = data.terraform_remote_state.service.outputs.container_registry_location
-#   resource_group_name      = data.terraform_remote_state.service.outputs.resource_group_name
-#   service_plan_name        = local.service_name
-#   service_plan_size        = var.service_plan_size
-#   service_plan_tier        = var.service_plan_tier
-#   hostname                 = "ui"
-#   dns_zone_name            = data.terraform_remote_state.service.outputs.subdomain
-# }
+module "ui" {
+  #source = "../../infra-terraform-modules/azure-app-service"
+  source                   = "git@github.com:ioet/infra-terraform-modules.git//azure-app-service?ref=tags/v0.0.13"
+  app_service_name         = "${local.service_name}-ui"
+  create_app_service_plan  = local.create_app_service_plan
+  docker_image_name        = "${local.image_name}:${var.image_tag}"
+  docker_image_namespace   = data.terraform_remote_state.service.outputs.container_registry_login_server
+  docker_registry_password = data.terraform_remote_state.service.outputs.container_registry_admin_password
+  docker_registry_url      = data.terraform_remote_state.service.outputs.container_registry_login_server
+  docker_registry_username = data.terraform_remote_state.service.outputs.container_registry_admin_username
+  location                 = data.terraform_remote_state.service.outputs.container_registry_location
+  resource_group_name      = data.terraform_remote_state.service.outputs.resource_group_name
+  service_plan_name        = local.service_name
+  service_plan_size        = var.service_plan_size
+  service_plan_tier        = var.service_plan_tier
+  hostname                 = "ui"
+  dns_zone_name            = data.terraform_remote_state.service.outputs.subdomain
+}
