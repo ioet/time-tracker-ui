a103db3cb3e153fcfceb80c8c14757c1280d0663
Rodolfo <rodolfo@no_email.com>
2022-08-21 10:14:58 -0500

    TTA-139 update tag version for ui module terrraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -43,7 +43,7 @@ locals {
 
 module "ui" {
   #source = "../../infra-terraform-modules/azure-app-service"
-  source                   = "git@github.com:ioet/infra-terraform-modules.git//azure-app-service?ref=TTA-139-deploy-production-environment-for-v-2"
+  source                   = "git@github.com:ioet/infra-terraform-modules.git//azure-app-service?ref=tags/v0.0.20"
   app_service_name         = "${local.service_name}-ui"
   create_app_service_plan  = local.create_app_service_plan
   docker_image_name        = "${local.image_name}:${var.image_tag}"
