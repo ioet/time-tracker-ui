7a6c5350b6a46453dd486f70553e404d69892988
Andres C <andresacg30@gmail.com>
2022-09-30 13:35:48 -0400

    fix tests errors

---
+++ src/app/modules/v2-redirect/v2-redirect.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-v2-redirect',
+  templateUrl: './v2-redirect.component.html',
+  styleUrls: ['./v2-redirect.component.css']
+})
+export class V2RedirectComponent {
+  
+}
