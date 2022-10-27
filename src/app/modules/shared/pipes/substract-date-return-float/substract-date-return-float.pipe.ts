4f42ff4aecd5ce34f3304b4ce31ba762097e4e0e
mmaquina <ignacio.lescano@ioet.com>
2022-10-27 20:42:08 -0300

    refactor: TTA-196 2 digits for fixed point duration of time entries in reports

--- src/app/modules/shared/pipes/substract-date-return-float/substract-date-return-float.pipe.ts
+++ src/app/modules/shared/pipes/substract-date-return-float/substract-date-return-float.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment';
 
-const FIXED_POINT_DIGITS = 1;
+const FIXED_POINT_DIGITS = 2;
 @Pipe({
   name: 'substractDateDisplayAsFloat'
 })
