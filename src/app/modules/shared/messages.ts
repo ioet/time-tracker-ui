371c358091f432d99908b1af5cef383da47b658a
Nicole Garcia <nicolsss>
2022-12-07 14:29:21 -0500

    refactor: inactivated project message

--- src/app/modules/shared/messages.ts
+++ src/app/modules/shared/messages.ts
@@ -1,3 +1,4 @@
 export const INFO_SAVED_SUCCESSFULLY = 'The data has been saved successfully';
 export const INFO_DELETE_SUCCESSFULLY = 'The data has been deleted successfully';
 export const UNEXPECTED_ERROR = 'An unexpected error happened, please try again later';
+export const PROJECT_DEACTIVATED_SUCCESSFULLY = 'The project has been inactivated successfully';
