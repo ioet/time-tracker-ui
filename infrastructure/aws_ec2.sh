60b09dd23b95f5006ce4f6fc0669552c9dd4cdf2
mmaquina <mmaquina@gmail.com>
2023-03-28 11:11:31 -0300

    fix aws env

--- infrastructure/aws_ec2.sh
+++ infrastructure/aws_ec2.sh
@@ -3,4 +3,4 @@ echo "Deploying $1..."
 docker ps -aq | xargs docker stop| xargs docker rm --force --volumes
 docker system prune -af
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 568748651446.dkr.ecr.us-east-1.amazonaws.com
-docker run -d --name timetracker_ui --env-file .$2.env -p 80:80 568748651446.dkr.ecr.us-east-1.amazonaws.com/time-tracker/$2-ui:$1
+docker run -d --name timetracker_ui --env-file .aws.$2.env -p 80:80 568748651446.dkr.ecr.us-east-1.amazonaws.com/time-tracker/$2-ui:$1
