ce0553ceb71ed846f237a79031d2b5c38a26ec1e
mmaquina <mmaquina@gmail.com>
2023-05-24 16:52:38 -0300

    test: TTL-888 added test for searching tech with special characters

--- src/app/modules/shared/services/technology.service.spec.ts
+++ src/app/modules/shared/services/technology.service.spec.ts
@@ -4,11 +4,12 @@ import { Technology } from '../../shared/models';
 import { TechnologyService } from './technology.service';
 import { STACK_EXCHANGE_ID, STACK_EXCHANGE_ACCESS_TOKEN } from '../../../../environments/environment';
 
-describe('TechnologyService', () => {
+fdescribe('TechnologyService', () => {
   let service: TechnologyService;
   let httpMock: HttpTestingController;
 
   const technologyList: Technology = { items: [{ name: 'java' }, { name: 'javascript' }] };
+  const technologyListWithSpecialChar: Technology = { items: [{ name: 'c#' }, { name: 'c#-2.0' }] };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -42,4 +43,18 @@ describe('TechnologyService', () => {
     expect(getTechnologiesRequest.request.method).toBe('GET');
     getTechnologiesRequest.flush(technologyList);
   });
+
+  it('technologies with special characters are read using GET from url', () => {
+    const technologyFoundSize = technologyListWithSpecialChar.items.length;
+    const technologyName = 'c#';
+    service.getTechnologies(technologyName).subscribe((technologyInResponse) => {
+      expect(technologyInResponse.items.length).toBe(technologyFoundSize);
+    });
+    const getTechnologiesRequest = httpMock.expectOne(
+      `${service.baseUrl}&inname=${encodeURIComponent(technologyName)}&site=stackoverflow&key=${STACK_EXCHANGE_ID}&access_token=${STACK_EXCHANGE_ACCESS_TOKEN}`
+    );
+    expect(getTechnologiesRequest.request.method).toBe('GET');
+    getTechnologiesRequest.flush(technologyListWithSpecialChar);
+  });
+
 });
