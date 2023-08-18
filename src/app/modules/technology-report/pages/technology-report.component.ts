f00bc1b9283f20e8f8564a01fa078f93e5a379c8
Santiago220991 <alexander.cardenas10@outlook.com>
2023-08-18 15:58:08 -0500

    feat: TTL-946 remove unused component

--- src/app/modules/technology-report/pages/technology-report.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-technology-report',
-  templateUrl: './technology-report.component.html'
-})
-export class TechnologyReportComponent {
-}
